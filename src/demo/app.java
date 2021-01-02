4ffffc02292e448b0ba5df327d10efc1301b6a4c
Admin_ZAk <azizi.zakaria@gmx.com>
2021-01-02 18:00:37 +0100

    add Product entitey

--- src/demo/app.java
+++ src/demo/app.java
@@ -1,9 +1,14 @@
 package demo;
 
+import BO.Product;
+
 public class app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Product  product = new Product();
+				
         System.out.println(" hello world  22222");
 		
 		
